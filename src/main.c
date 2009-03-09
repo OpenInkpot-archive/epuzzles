0b1003aec9aa5e6b52c2badc696e61d27e7f8dc6
avn <avn@frog.(none)>
2009-03-10 01:16:27 +0200

    EWL skeleton

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <Ewl.h>
+
+#define PROG    "EWL Puzzles"
+
+/* globals */
+static Ewl_Widget *main_win = NULL;
+static Ewl_Widget *fd_win = NULL;
+
+
+/* lets go */
+int main(int argc, char ** argv) {
+    ewl_init(&argc, argv);
+    init_gui();
+    ewl_main();
+    return 0;
+}
+
