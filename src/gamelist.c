95f6eaa0921a943b5c47552c66bae482fbe347c1
IPlinux build user <build@frog.(none)>
2009-04-01 08:25:00 +0000

    Rework gamelist initialization

--- src/gamelist.c
+++ src/gamelist.c
@@ -3,7 +3,17 @@
 
 extern game fifteen;
 
+const game  *gamelist[] = {
+    &fifteen,
+    0,
+};
+
 void
 setup_gamelist(Ewl_Widget *w) {
-   append_game(w, &fifteen);
+    struct game **each;
+    each = gamelist;
+    while(*each){
+       append_game(w, *each);
+       ++each;
+    }
 }
