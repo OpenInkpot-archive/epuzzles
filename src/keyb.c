9d83b6bf7eeec0fc3f02d6d67bde68f1f4f389e6
Alexander V. Nikolaev <avn@daemon.hole.ru>
2009-05-26 11:53:29 +0000

    Key mapping

--- src/keyb.c
+++ src/keyb.c
@@ -20,27 +20,26 @@ static void _key_handler(Ewl_Widget* w, void *event, void *context) {
     printf("In handler %s\n", k);
 
 #define HANDLE_KEY(kk, vv)  if( !strcmp(k, kk) ){ keyval = vv; }
-    HANDLE_KEY("9", MOD_NUM_KEYPAD | '9') else
-    HANDLE_KEY("Page_Up", MOD_NUM_KEYPAD | '9') else
-    HANDLE_KEY("8",   CURSOR_UP) else
+    HANDLE_KEY("KP_0", MOD_NUM_KEYPAD | '0') else
+    HANDLE_KEY("KP_9", MOD_NUM_KEYPAD | '9') else
+    HANDLE_KEY("KP_8", MOD_NUM_KEYPAD | '8') else
+    HANDLE_KEY("KP_7", MOD_NUM_KEYPAD | '7') else
+    HANDLE_KEY("KP_6", MOD_NUM_KEYPAD | '6') else
+    HANDLE_KEY("KP_5", MOD_NUM_KEYPAD | '5') else
+    HANDLE_KEY("KP_4", MOD_NUM_KEYPAD | '4') else
+    HANDLE_KEY("KP_3", MOD_NUM_KEYPAD | '3') else
+    HANDLE_KEY("KP_2", MOD_NUM_KEYPAD | '2') else
+    HANDLE_KEY("KP_1", MOD_NUM_KEYPAD | '1') else
     HANDLE_KEY("Up",  CURSOR_UP) else
-    HANDLE_KEY("7",  MOD_NUM_KEYPAD | '7') else
-    HANDLE_KEY("Home",  MOD_NUM_KEYPAD | '7') else
-    HANDLE_KEY("6",  CURSOR_RIGHT) else
     HANDLE_KEY("Right",  CURSOR_RIGHT) else
-    HANDLE_KEY("5",  MOD_NUM_KEYPAD | '5') else
-    // FIXME: equivalent of GDK_KP_Begin
-    HANDLE_KEY("Begin",  MOD_NUM_KEYPAD | '0') else
-    HANDLE_KEY("4",  CURSOR_LEFT) else
     HANDLE_KEY("Left",  CURSOR_LEFT) else
-    HANDLE_KEY("3",  MOD_NUM_KEYPAD | '3') else
-    HANDLE_KEY("Page_Down",  MOD_NUM_KEYPAD | '3') else
-    HANDLE_KEY("2",  CURSOR_DOWN) else
     HANDLE_KEY("Down",  CURSOR_DOWN) else
-    HANDLE_KEY("1",  MOD_NUM_KEYPAD | '1') else
-    HANDLE_KEY("End",  MOD_NUM_KEYPAD | '1') else
-    HANDLE_KEY("0",  MOD_NUM_KEYPAD | '0') else
-    HANDLE_KEY("Insert",  MOD_NUM_KEYPAD | '0') else
+
+    /* For emulation on PC */
+    HANDLE_KEY("j",  CURSOR_UP) else
+    HANDLE_KEY("h",  CURSOR_RIGHT) else
+    HANDLE_KEY("l",  CURSOR_LEFT) else
+    HANDLE_KEY("k",  CURSOR_DOWN) else
     if(!strcmp(k,"Enter") || !strcmp(k,"Return")) {
         keyval=CURSOR_SELECT;
     } else
